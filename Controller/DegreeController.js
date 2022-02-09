81b89e1641b681cce79d20abb2b36ce50ae524c5
Ong Wen Kai <ongwenkai92@gmail.com>
2022-02-09 22:44:12 +0800

    progress on DegreeRoute - create getDegree and postDegree

--- Controller/DegreeController.js
+++ Controller/DegreeController.js
@@ -1,12 +1,12 @@
-const { poolPromise } = require("../database.js");
+const { poolPromise, sql } = require("../database.js");
 class DegreeController {
-  static async getDegree(req, res) {
+  static async getDegreeByCampus(req, res, next) {
     try {
-      const campus = req.query.campus;
+      const campusId = req.query.campusId;
       const pool = await poolPromise;
       const result = await pool
         .request()
-        .input("campus_id", campus)
+        .input("campus_id", campusId)
         .query(
           "SELECT Degree.Degree_ID,Degree_Name FROM Degree " +
             "inner join Campus_Degree  ON  Degree.Degree_ID = Campus_Degree.Degree_ID " +
@@ -22,6 +22,43 @@ class DegreeController {
     } catch (error) {
       console.log(error);
       res.json({ status: "failed", message: error.message });
+    }
+  }
+  static async getDegreeById(req, res, next) {
+    try {
+      const degreeId = req.params.degreeId;
+
+      const pool = await poolPromise;
+      const result = await pool
+        .request()
+        .input("Degree_Id", degreeId)
+        .query("SELECT * FROM Degree WHERE Degree_ID = @Degree_Id");
+
+      const { recordset, rowsAffected } = result;
+      res.json({ result: recordset[0], rowsAffected: rowsAffected[0] });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ status: "failed", message: error.message });
+    }
+  }
+  static async addDegree(req, res, next) {
+    try {
+      const { degreeId, degreeName, totalCredit, minYear, maxYear } = req.body;
+      const pool = await poolPromise;
+      const result = await pool
+        .request()
+        .input("Degree_Id", degreeId)
+        .input("Degree_Name", degreeName)
+        .input("Total_Credit", totalCredit)
+        .input("Min_Year", minYear)
+        .input("Max_Year", maxYear)
+        .query(
+          "INSERT INTO Degree VALUES(@Degree_Id,@Degree_Name,@Total_Credit,@Min_Year,@Max_Year)"
+        );
+
+      res.json({ status: "success" });
+    } catch (error) {
+      res.status(400).json({ status: "failed", error: error.message });
     }
   }
 }
--- Routes/DegreeRoute.js
+++ Routes/DegreeRoute.js
@@ -1,6 +1,9 @@
 const DegreeController = require("../Controller/DegreeController");
 
 const DegreeRouter = require("express").Router();
-DegreeRouter.get("/", DegreeController.getDegree);
+DegreeRouter.get("/:degreeId", DegreeController.getDegreeById);
+DegreeRouter.route("/")
+  .get(DegreeController.getDegreeByCampus)
+  .post(DegreeController.addDegree);
 
 module.exports = DegreeRouter;
